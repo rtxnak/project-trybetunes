330e934bc59d3e5953969994f2a5b61c0555bede
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-11 16:03:52 -0300

    feat: adicionou rotas e pages conforme requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <div>
+          <Switch>
+            <Route path="/" exact component={ Login } />
+            <Route path="/search" exact component={ Search } />
+            <Route path="/Album/:id" exact component={ Album } />
+            <Route path="/favorites" exact component={ Favorites } />
+            <Route path="/profile" exact component={ Profile } />
+            <Route path="/profile/edit" exact component={ ProfileEdit } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
