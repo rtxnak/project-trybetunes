05657a3a0f2b8e9d8256f27981d88ce683a32e10
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-13 20:22:49 -0300

    feat: adicionou formulario para identificacao no arquivo Login.jsx , renderizacao de carregamento no Loading.jsx e acao de redirecionamento para /search conforme requisito 2

---
+++ src/components/Content.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+import Search from '../pages/Search';
+import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import NotFound from '../pages/NotFound';
+import { createUser } from '../services/userAPI';
+
+class Content extends React.Component {
+  constructor() {
+    super();
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onClickButton = this.onClickButton.bind(this);
+    this.state = {
+      loginNameInput: '',
+      onLoad: false,
+      createUserIsOk: false,
+    };
+  }
+
+  async onClickButton() {
+    const { loginNameInput } = this.state;
+    this.setState({
+      onLoad: true,
+    });
+    await createUser({ name: loginNameInput });
+    this.setState({ createUserIsOk: true });
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    const { value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const {
+      loginNameInput,
+      onLoad,
+      createUserIsOk,
+    } = this.state;
+
+    return (
+      <main className="Content">
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={ () => (<Login
+              loginNameInput={ loginNameInput }
+              onLoad={ onLoad }
+              createUserIsOk={ createUserIsOk }
+              onInputChange={ this.onInputChange }
+              onClickButton={ this.onClickButton }
+            />) }
+          />
+          <Route path="/search" exact component={ Search } />
+          <Route path="/Album/:id" exact component={ Album } />
+          <Route path="/favorites" exact component={ Favorites } />
+          <Route path="/profile" exact component={ Profile } />
+          <Route path="/profile/edit" exact component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </main>
+    );
+  }
+}
+
+export default Content;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
+import Loading from './Loading';
 
 class Login extends Component {
   render() {
+    const {
+      loginNameInput,
+      onLoad,
+      createUserIsOk,
+      onInputChange,
+      onClickButton,
+    } = this.props;
+
+    const THREE = 3;
+    const isButtonDisabled = loginNameInput.length < THREE;
+
     return (
-      <div
-        data-testid="page-login"
-      >
-        login
+      <div data-testid="page-login">
+        <input
+          name="loginNameInput"
+          data-testid="login-name-input"
+          type="text"
+          value={ loginNameInput }
+          onChange={ onInputChange }
+        />
+        <button
+          disabled={ isButtonDisabled }
+          data-testid="login-submit-button"
+          onClick={ onClickButton }
+          type="submit"
+        >
+          Entrar
+        </button>
+        {onLoad && <Loading />}
+        {createUserIsOk && <Redirect to="/search" />}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  loginNameInput: PropTypes.string.isRequired,
+  onLoad: PropTypes.bool.isRequired,
+  createUserIsOk: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onClickButton: PropTypes.func.isRequired,
+};
 
 export default Login;
