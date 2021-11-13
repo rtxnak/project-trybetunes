715606f79bc4795311341c2e897b1f0d69d1d885
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-13 19:01:24 -0300

    fix: reorganizou transferindo as rotas para o arquivo Content.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Search from './pages/Search';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
-import NotFound from './pages/NotFound';
+import { BrowserRouter } from 'react-router-dom';
+import Content from './Components/Content';
 
 class App extends React.Component {
   render() {
@@ -14,15 +8,7 @@ class App extends React.Component {
       <BrowserRouter>
         <p>TrybeTunes</p>
         <div>
-          <Switch>
-            <Route path="/" exact component={ Login } />
-            <Route path="/search" exact component={ Search } />
-            <Route path="/Album/:id" exact component={ Album } />
-            <Route path="/favorites" exact component={ Favorites } />
-            <Route path="/profile" exact component={ Profile } />
-            <Route path="/profile/edit" exact component={ ProfileEdit } />
-            <Route path="*" component={ NotFound } />
-          </Switch>
+          <Content />
         </div>
       </BrowserRouter>
     );
