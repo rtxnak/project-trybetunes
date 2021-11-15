a188ab6f4e7efcdafd852a6562eec8939e17df88
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-15 00:26:38 -0300

    feat: adicionou funcao de pesquisar artista na pagina search conforme requsito 6

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,6 +8,7 @@ import Profile from '../pages/Profile';
 import ProfileEdit from '../pages/ProfileEdit';
 import NotFound from '../pages/NotFound';
 import { createUser } from '../services/userAPI';
+import searchAlbumsAPIs from '../services/searchAlbumsAPI';
 
 class Content extends React.Component {
   constructor() {
@@ -15,21 +16,17 @@ class Content extends React.Component {
 
     this.onInputChange = this.onInputChange.bind(this);
     this.onClickButton = this.onClickButton.bind(this);
+    this.onClickSearchButton = this.onClickSearchButton.bind(this);
+    this.searchAlbums = this.searchAlbums.bind(this);
+
     this.state = {
       loginNameInput: '',
       onLoad: false,
       createUserIsOk: false,
       searchInput: '',
+      onSearchInput: '',
+      searchedAlbums: [],
     };
-  }
-
-  async onClickButton() {
-    const { loginNameInput } = this.state;
-    this.setState({
-      onLoad: true,
-    });
-    await createUser({ name: loginNameInput });
-    this.setState({ createUserIsOk: true });
   }
 
   onInputChange({ target }) {
@@ -40,12 +37,46 @@ class Content extends React.Component {
     });
   }
 
+  async onClickButton() {
+    const { loginNameInput } = this.state;
+    this.setState({
+      onLoad: true,
+    });
+    await createUser({ name: loginNameInput });
+    this.setState({ createUserIsOk: true });
+    this.setState({
+      onLoad: false,
+    });
+  }
+
+  async onClickSearchButton() {
+    const { searchInput } = this.state;
+    this.setState({
+      onLoad: true,
+      onSearchInput: searchInput,
+    });
+    await this.searchAlbums();
+  }
+
+  async searchAlbums() {
+    const { searchInput } = this.state;
+    const searchAlbumsResult = await searchAlbumsAPIs(searchInput);
+    // console.log(searchAlbumsResult);
+    this.setState({
+      searchedAlbums: [...searchAlbumsResult],
+      onLoad: false,
+      searchInput: '',
+    });
+  }
+
   render() {
     const {
       loginNameInput,
       onLoad,
       createUserIsOk,
       searchInput,
+      searchedAlbums,
+      onSearchInput,
     } = this.state;
 
     return (
@@ -54,21 +85,29 @@ class Content extends React.Component {
           <Route
             path="/"
             exact
-            render={ () => (<Login
-              loginNameInput={ loginNameInput }
-              onLoad={ onLoad }
-              createUserIsOk={ createUserIsOk }
-              onInputChange={ this.onInputChange }
-              onClickButton={ this.onClickButton }
-            />) }
+            render={
+              () => (<Login
+                loginNameInput={ loginNameInput }
+                onLoad={ onLoad }
+                createUserIsOk={ createUserIsOk }
+                onInputChange={ this.onInputChange }
+                onClickButton={ this.onClickButton }
+              />)
+            }
           />
           <Route
             path="/search"
             exact
-            render={ () => (<Search
-              searchInput={ searchInput }
-              onInputChange={ this.onInputChange }
-            />) }
+            render={
+              () => (<Search
+                searchInput={ searchInput }
+                onLoad={ onLoad }
+                onInputChange={ this.onInputChange }
+                onClickSearchButton={ this.onClickSearchButton }
+                searchedAlbums={ searchedAlbums }
+                onSearchInput={ onSearchInput }
+              />)
+            }
           />
           <Route path="/Album/:id" exact component={ Album } />
           <Route path="/favorites" exact component={ Favorites } />
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 
 class Search extends Component {
   render() {
     const {
       searchInput,
       onInputChange,
+      onClickSearchButton,
+      onLoad,
+      searchedAlbums,
+      onSearchInput,
     } = this.props;
+
+    const searchedAlbumsNotEmpty = searchedAlbums.length > 0;
 
     const TWO = 2;
     const isButtonDisabled = searchInput.length < TWO;
@@ -17,22 +25,43 @@ class Search extends Component {
         data-testid="page-search"
       >
         <Header />
+        {onLoad ? <Loading /> : (
+          <>
+            <input
+              name="searchInput"
+              data-testid="search-artist-input"
+              type="text"
+              value={ searchInput }
+              onChange={ onInputChange }
+            />
+            <button
+              disabled={ isButtonDisabled }
+              data-testid="search-artist-button"
+              onClick={ onClickSearchButton }
+              type="submit"
+            >
+              Pesquisar
+            </button>
+          </>
+        )}
 
-        <input
-          name="searchInput"
-          data-testid="search-artist-input"
-          type="text"
-          value={ searchInput }
-          onChange={ onInputChange }
-        />
-        <button
-          disabled={ isButtonDisabled }
-          data-testid="search-artist-button"
-          // onClick={}
-          type="submit"
-        >
-          Pesquisar
-        </button>
+        {
+          onSearchInput
+            ? <p>{`Resultado de álbuns de: ${onSearchInput}`}</p> : ''
+        }
+
+        { !searchedAlbumsNotEmpty && onSearchInput && <p>Nenhum álbum foi encontrado</p> }
+
+        { searchedAlbumsNotEmpty && searchedAlbums.map((album) => (
+          <li key={ album.collectionId }>
+            <Link
+              to={ `/album/${album.collectionId}` }
+              data-testid={ `link-to-album-${album.collectionId}` }
+            >
+              <span>{album.collectionName}</span>
+            </Link>
+          </li>
+        ))}
       </div>
     );
   }
@@ -40,7 +69,11 @@ class Search extends Component {
 
 Search.propTypes = {
   searchInput: PropTypes.string.isRequired,
+  onLoad: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
+  onClickSearchButton: PropTypes.func.isRequired,
+  searchedAlbums: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onSearchInput: PropTypes.string.isRequired,
 };
 
 export default Search;
